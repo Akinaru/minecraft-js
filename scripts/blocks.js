a4e091b70990f71406e59a2214aa1d03fa213f2e
Akinaru <90217410+Akinaru@users.noreply.github.com>
2023-11-26 15:53:16 +0100

    Add files via upload

---
+++ scripts/blocks.js
@@ -0,0 +1,23 @@
+export const blocks = {
+    empty: {
+      id: 0,
+      name: 'empty'  
+    },
+    grass: {
+        id: 1,
+        name: 'grass', 
+        color: 0x559020
+    },
+    dirt: {
+        id: 2,
+        name: 'dirt',
+        color:  0X807020
+    },
+    stone: {
+        id: 3,
+        name: 'stone',
+        color: 0x808080,
+        scale: {x: 30, y: 30, z: 30},
+        scarcity: 0.5 
+    }
+}
